879bb0167914187b8e12cddc6fc666921f31cc09
GamerNoTitle <admin@bili33.top>
2022-03-11 18:38:23 +0800

    Brandly new parameter, this will return a json

--- server.py
+++ server.py
@@ -30,11 +30,10 @@ def parser(query):
     parameter = path[0]
     if parameter not in paths:   # When the path not exists, this will return
         return json.dumps(Error404)
-    if parameter == 'song':
+    if parameter == 'song': 
         id = request.args.get('id')
-        file, song, author = NeteaseDownload(id)
-        if file:
-            return flask.send_from_directory('./song/AppData/', file, as_attachment=False, download_name='{} - {}.mp3'.format(author, song))
+        ContentType = request.args.get('type')
+        return NeteaseHandler(id, ContentType)
     if parameter == 'clear':
         msg = Clear()
         return msg
@@ -42,6 +41,21 @@ def parser(query):
 @app.errorhandler(404)
 def not_found(e):
     return json.dumps(Error404)
+
+def NeteaseHandler(id, ContentType):
+    if ContentType != 'attachment' and ContentType != 'json': ContentType = 'attachment'
+    if ContentType == 'attachment':
+        file, song, author = NeteaseDownload(id, ContentType)
+        if file:
+            return flask.send_from_directory('./song/AppData/', file, as_attachment=False, download_name='{} - {}.mp3'.format(author, song))
+        else:
+            if song == 404 and author == 404:
+                return {'code': -1, 'msg': 'Cannot fetch resource.'}
+            elif song == 'Not Found' and author == 'Not Found':
+                return {'code': -2, 'msg': 'It seems that id {} is not a valid song id.'.format(id)}
+    else:
+        Info = NeteaseDownload(id, ContentType)
+        return Info
 
 
 app.run(host='0.0.0.0', port=8080, debug=True)
--- utils/NeteaseCloudMusic.py
+++ utils/NeteaseCloudMusic.py
@@ -4,36 +4,63 @@ import os
 import re
 
 
-def NeteaseDownload(id):
-    song, author = FileInfo(id)
-    base = 'https://music.163.com/song/media/outer/url?id='
-    file = './song/AppData/' + id + '.mp3'
-    filename = id+'.mp3'
-    if os.path.exists(file):
-        return filename, song, author
-    with open(file, 'wb') as f:
+def NeteaseDownload(id, ContentType):
+    if ContentType == 'attachment':
+        song, author = FileInfo(id)
+        if song == None and author == None:     # If the song id doesn't map to a song, this will return.
+            return False, 'Not Found', 'Not Found'
+        base = 'https://music.163.com/song/media/outer/url?id='
+        file = './song/AppData/' + id + '.mp3'
+        filename = id+'.mp3'
+        if os.path.exists(file):
+            return filename, song, author
         try:
             stream = r.get(base + id, timeout=30)
-            print('[NETEASEMUSICDOWNLOAD]Getting song {} - {}, returned status_code {}'.format(author, song, stream.status_code))
-            f.write(stream.content)
         except TimeoutError:
             return False, None, None
-    return filename, song, author
+        if 'u-errlg u-errlg-404' in stream.text:    # Some VIP songs cannot be downloaded
+            print('[NETEASEMUSICDOWNLOAD] Failed while getting the resources and information of a song.'.format(author, song, stream.status_code))
+            return False, 404, 404
+        print('[NETEASEMUSICDOWNLOAD] Getting song {} - {}, returned status_code {}'.format(author, song, stream.status_code))
+        with open(file, 'wb') as f:
+            f.write(stream.content)
+        return filename, song, author 
+    else:
+        base = 'https://music.163.com/song/media/outer/url?id='
+        song, author = FileInfo(id)
+        if song == None and author == None:
+            return {'code': -2, 'msg': 'It seems that id {} is not a valid song id.'.format(id)}
+        try:
+            stream = r.get(base + id, timeout=30)
+        except TimeoutError:
+            return {'code': -3, 'msg': 'Timed out. Please try again later.'}
+        if stream.url == "https://music.163.com/404": return {'code': 200, 'link': None, 'name': song, 'author': author, 'msg': 'Cannot fetch download link.'}
+        return {'code': 200, 'link': stream.url, 'name': song, 'author': author, 'msg': 'Success'}
+        
+
+
+    
 
 
 def FileInfo(id):   # Get the information of the song, including name and author
     TargetWebInfo=r.get('https://music.163.com/song?id=' + id)
     html=TargetWebInfo.content
     bf=bs4.BeautifulSoup(html, "lxml")
-    song=str(list(bf.find_all('em', class_="f-ff2"))[0])
-    author=str(list(bf.find_all('a', class_='s-fc7'))[1])
-    song=song.replace('<em class="f-ff2">', '').replace('</em>', '')
-    ReplaceLink=re.findall(r'<a class="s-fc7" href=".+">', author)
-    for i in ReplaceLink:
-        author=author.replace(i, '')
-    author=author.replace('</a>', '')
-    ForbiddenCharacters=[('\\', ' '), ('/', ' '), (':', '：'),
+    try:
+        song=str(list(bf.find_all('em', class_="f-ff2"))[0])
+        song=song.replace('<em class="f-ff2">', '').replace('</em>', '')
+        ForbiddenCharacters=[('\\', ' '), ('/', ' '), (':', '：'),
                            ('*', ' '), ('?', '？'), ('<', ' '), ('>', ' '), ('|', '丨')]
-    for i in ForbiddenCharacters:
-        song=song.replace(i[0], i[1])
+        for i in ForbiddenCharacters:
+            song=song.replace(i[0], i[1])
+    except: song = None
+    try:
+        author=str(list(bf.find_all('a', class_='s-fc7'))[1])
+        ReplaceLink=re.findall(r'<a class="s-fc7" href=".+">', author)
+        for i in ReplaceLink:
+            author=author.replace(i, '')
+        author=author.replace('</a>', '')
+        if author == '${escape(x.beRepliedUser.nickname)}': author = None
+    except:
+        author = None
     return song, author
